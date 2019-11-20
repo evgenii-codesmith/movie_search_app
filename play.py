475e40a3e877ca816ded67c4152c0edf42fc243a
evgenii-codesmith <evgenii.codesmith@gmail.com>
2019-11-20 21:36:08 +0300

    Refactored using list comp

--- play.py
+++ play.py
@@ -4,26 +4,17 @@ import collections
 search = input('Enter movie to search:')
 url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
 resp = requests.get(url)
+resp.raise_for_status()
 
-movie_data = resp.json()['hits']
+movie_list = resp.json().get('hits')
 
-MovieRSesult = collections.namedtuple(
-    'MovieResult', "imdb_code, title, director, duration, genres, rating, year, imdb_score")
 
-movies = []
-for item in movie_data:
-    m = MovieRSesult(
-        imdb_code=item.get('imdb_code'),
-        title=item.get('title'),
-        director=item.get('director'),
-        duration=item.get('duration'),
-        genres=item.get('genres'),
-        rating=item.get('rating', 0),
-        year=item.get('year', 0),
-        imdb_score=item.get('imdb_score', 0.0)
+MovieResult = collections.namedtuple(
+    'MovieResult', "imdb_code, title, duration, director, year, rating, imdb_score, keywords, genres")
 
-    )
-    movies.append(m)
+movies = [
+    MovieResult(**movie)
+    for movie in movie_list]
 
 print('Found {} movies for search {}'.format(len(movies), search))
 for m in movies:
